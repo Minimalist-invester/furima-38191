2e48dd94699e725a2aef306d68e89c30390003f2
ryou <105363911+Minimalist-investor@users.noreply.github.com>
2022-08-09 09:05:15 +0900

    user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.includes(:user).order('created_at DESC')
+    #@items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
-    @item = Item.new
+   # @item = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
+    #@item = Item.new(item_params)
+    #if @item.save
+     # redirect_to root_path
+    #else
+     # render :new
     end
   end
 
