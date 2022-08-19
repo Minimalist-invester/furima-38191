c65577cdb8e1ffcc8d3fb0d52aa2bd27069bd964
ryou <105363911+Minimalist-investor@users.noreply.github.com>
2022-08-19 16:52:32 +0900

    s3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
- ActiveRecord::Schema.define(version: 2022_08_17_090103) do
+ActiveRecord::Schema.define(version: 2022_08_17_090103) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -76,18 +76,18 @@
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-   create_table "order_forms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-     t.datetime "created_at", precision: 6, null: false
-     t.datetime "updated_at", precision: 6, null: false
-   end
+  create_table "order_forms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
-   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-     t.datetime "created_at", precision: 6, null: false
-     t.datetime "updated_at", precision: 6, null: false
-     t.bigint "user_id", null: false
-     t.bigint "item_id", null: false
-     t.index ["item_id"], name: "index_orders_on_item_id"
-     t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "ship_dates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
